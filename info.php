e94131bc6abf44e7ed5e2ce8be513e224e89772e
Jacob G <jacob-x@jvvgindustries.com>
2017-01-10 10:39:45 -0500

    first commit

---
+++ info.php
@@ -0,0 +1,8 @@
+<?php
+$ext_info = array(
+	'title'			=> 'BBCode Toolbar',
+	'website'		=> 'http://futurebb.futuresight.org/extensions/bbcodebuttons',
+	'support'		=> 'http://futuresight.org/support',
+	'minversion'	=> 1.3,
+	'uninstallable'	=> true,
+);
